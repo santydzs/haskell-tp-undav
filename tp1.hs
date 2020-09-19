2e436d5efdad2c8f2fcf8e98eb996b437c9d86a7
Santiago Alessio <santii.dev@gmail.com>
2020-09-19 18:56:23 -0300

    se sube punto 3

--- tp1.hs
+++ tp1.hs
@@ -2,10 +2,17 @@ data Super = Heroe { nombre :: String, poder :: Float, vida :: Float} | Villano 
 
 participantes = [Heroe "batman" 2000 300000, Villano "megamente" 1800 50000 9000, Heroe "ironman" 1500 5030202, Heroe "Goku" 100000000000000 1010101010110101010, Heroe "KungFu Panda" 20  202020202, Villano "Pinguino" 1010 20202020202 253]
 
+--punto 2
 poderReal (Heroe _ poder _) = poder
 poderReal (Villano _ poder _ maldad) = poder - (maldad / 6)
 
+-- punto 3
 over9000 = (9000<).poderReal
+
+--punto 4
+reducirvida vida = (vida-).poderReal
+
+--golpear (Heroe _ _ _) (Villano nombre vpoder vida maldad) = (Villano nombre vpoder (reducirvida vida (Heroe _ _ _)) maldad) 
 
 megamente = Villano "megamente" 1800 50000 9000
 main = putStrLn (show (over9000 megamente))
