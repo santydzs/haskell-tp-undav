3073e287b9b122f42adc6029c08b337812310112
Santiago Alessio <santii.dev@gmail.com>
2020-09-19 20:47:37 -0300

    se agrega punto 4

--- tp1.hs
+++ tp1.hs
@@ -1,4 +1,4 @@
-data Super = Heroe { nombre :: String, poder :: Float, vida :: Float} | Villano { nombre :: String, poder :: Float, vida :: Float, maldad :: Float}
+data Super = Heroe { nombre :: String, poder :: Float, vida :: Float} | Villano { nombre :: String, poder :: Float, vida :: Float, maldad :: Float} deriving (Show)
 
 participantes = [Heroe "batman" 2000 300000, Villano "megamente" 1800 50000 9000, Heroe "ironman" 1500 5030202, Heroe "Goku" 100000000000000 1010101010110101010, Heroe "KungFu Panda" 20  202020202, Villano "Pinguino" 1010 20202020202 253]
 
@@ -11,8 +11,11 @@ over9000 = (9000<).poderReal
 
 --punto 4
 reducirvida vida = (vida-).poderReal
+golpear heroe (Villano nombre poder vida maldad) = (Villano nombre poder (reducirvida vida heroe) maldad) 
+golpear villano (Heroe nombre poder vida) = (Heroe nombre poder (reducirvida vida villano))
 
---golpear (Heroe _ _ _) (Villano nombre vpoder vida maldad) = (Villano nombre vpoder (reducirvida vida (Heroe _ _ _)) maldad) 
-
+--test
+batman = Heroe "batman" 2000 300000
 megamente = Villano "megamente" 1800 50000 9000
-main = putStrLn (show (over9000 megamente))
+
+main = putStrLn (show (golpear megamente batman) )
