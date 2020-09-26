96474a4ca045415e3a0928c62df6c2776e889226
Santiago Alessio <santii.dev@gmail.com>
2020-09-26 14:17:50 -0300

    se hacen correciones luego de realizar las pruebas, primera version entregable

--- tp1.hs
+++ tp1.hs
@@ -1,6 +1,7 @@
-data Super = Heroe { nombre :: String, poder :: Float, vida :: Float} | Villano { nombre :: String, poder :: Float, vida :: Float, maldad :: Float} deriving (Show)
+--necesario para utlizar la funcion delete, la cual recibe un elemento y una lista y si esta, elimina la primer aparicion del elemento en la lista
+import Data.List
 
-participantes = [Heroe "batman" 2000 300000, Villano "megamente" 1800 50000 9000, Villano "Thanos" 100000 50000 9000 , Heroe "ironman" 1500 5030202, Heroe "Goku" 100000000000000 1010101010110101010, Heroe "KungFu Panda" 20  202020202, Villano "Pinguino" 1010 20202020202 253]
+data Super = Heroe { nombre :: String, poder :: Float, vida :: Float} | Villano { nombre :: String, poder :: Float, vida :: Float, maldad :: Float} deriving (Show, Eq)
 
 --punto 2
 poderReal :: Super -> Float
@@ -20,20 +21,36 @@ golpear super (Heroe nombre poder vida) = (Heroe nombre poder (reducirvida vida 
 
 --punto 5
 ganadorCombate :: Super -> Super -> Super
-ganadorCombate super1 super2 | 0 >= (vida super1) = super2
-                            | 0 >= (vida super2) = super1
+ganadorCombate super1 super2 | 0 >= (vida super2) = super1
+                            | 0 >= (vida super1) = super2
                             | otherwise = ganadorCombate (golpear super2 super1) (golpear super1 super2)
 
 --punto 6
 ataquePandilla :: Foldable t => Super -> t Super -> Super
-ataquePandilla super pandilla = foldl golpear super pandilla
+ataquePandilla super pandilla = foldl (\ supergolpeado pandillero -> golpear pandillero supergolpeado) super pandilla
 
 --punto 7
 estadisticas :: (Super -> Bool) -> [Super] -> [String]
 estadisticas funcion lista = map nombre (filter funcion lista)
 
 --test
-batman = Heroe "batman" 1000 5000
-megamente = Villano "megamente" 1500 10000 6
 
-main = putStrLn (show (ganadorCombate megamente batman) )
+--punto 7 preubas
+punto2testA = estadisticas over9000 pandilla1
+punto2testB = estadisticas over9000 pandilla2
+punto2testC = estadisticas (\ pandillero -> 0 < (vida (ataquePandilla pandillero (delete pandillero pandilla2)))) pandilla2
+punto2testD = estadisticas (\ pandillero -> 0 < (vida (ataquePandilla pandillero (delete pandillero pandilla2)))) pandilla3
+
+pandilla1 = [superman,ironman,goku]
+pandilla2 = [ironman,joker,goku]
+pandilla3 = [ironman,joker,goku,vegeta]
+
+goku = Heroe "goku" 67888000 5000
+superman = Heroe "superman" 10000 5030202
+thanos = Villano "thanos" 1000000 10000 6
+joker = Villano "joker" 500 5000 3000
+ironman = Heroe "ironman" 1500 5030202
+vegeta = Heroe "vegeta" 1000 77888000
+
+
+main = putStrLn (show punto2testD )
